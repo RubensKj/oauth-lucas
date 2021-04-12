da692143bde09e80f125bdbb6e8496c02bc11290
RubensKj <rubens.junior@ambevtech.com.br>
2021-04-12 14:48:46 -0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import api from './api';
 
 function App() {
+
+  function handle() {
+    api.get('/auth').then(respose => {
+      console.log(respose);
+    }).catch(response => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handle}>
+        Click to oAuth
+      </button>
     </div>
   );
 }
